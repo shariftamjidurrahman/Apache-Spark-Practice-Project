2b357e50c4f7e5b10c4c74eed7bcadb40472f47d
shariftamjidurrahman <sharifrahman@gmail.com>
2024-06-18 22:42:57 +0600

    First commit of apache Spark Project

---
+++ src/main/java/com/example/ApacheSparkPractice/ApacheSparkPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApacheSparkPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheSparkPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheSparkPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApacheSparkPractice/ApacheSparkPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApacheSparkPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheSparkPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
